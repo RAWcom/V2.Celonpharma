432b102286117f9170a47f32b7e1a7cd5e401900
fraczo <jacek.rawiak@hotmail.com>
2016-05-20 12:02:43 +0200

    published
    przestawiony scope z site na web - ale i tak wyłączona funkcjonalność na
    witrynie

---
+++ V2.Celonpharma/Backup1/BLL/SPEmail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint;
+using System.Net.Mail;
+using System.Collections.Specialized;
+using Microsoft.SharePoint.Utilities;
+using System.Net;
+
+namespace SPEmail
+{
+    public class EmailGenerator
+    {
+        public static void SendMail(SPWeb web, string from, string to, string subject, string body, bool isBodyHtml, string cc, string bcc)
+        {
+            StringDictionary headers = new StringDictionary();
+            headers.Add("from", from);
+            headers.Add("to", to);
+            headers.Add("subject", subject);
+            if (!String.IsNullOrEmpty(cc)) headers.Add("cc", cc);
+            if (!String.IsNullOrEmpty(bcc)) headers.Add("bcc", bcc);
+            headers.Add("content-type", "text/html");
+            SPUtility.SendEmail(web, headers, body);
+        }
+    }
+}
---
+++ V2.Celonpharma/Backup1/BLL/Tools.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint;
+using System.Net.Mail;
+
+namespace BLL
+{
+    public class Tools
+    {
+        //Email
+
+        const string sender = @"noreply@stafix24.pl";
+
+        public static void SendWarningMessageToGroup(SPWeb web, string groupName, string subject, string bodyHTML)
+        {
+            SPGroupCollection groupCollection = web.SiteGroups;
+            foreach (SPGroup group in groupCollection)
+            {
+                if (group.Name == groupName)
+                {
+                    string sender1 = sender;
+                    if (!string.IsNullOrEmpty(web.Site.RootWeb.Title)) sender1 = string.Format(@"{1}<{0}>",
+                                                                                    sender,
+                                                                                    web.Site.RootWeb.Title);
+                    
+
+#if DEBUG
+                    SPEmail.EmailGenerator.SendMail(web, sender1, web.CurrentUser.Email, subject, bodyHTML, true, string.Empty, string.Empty);
+#else                          
+                    SPUserCollection userCollection = group.Users;
+                    foreach (SPUser user in userCollection)
+                    {
+                        if (IsValidEmailAddress(user.Email))
+                        {
+                            SPEmail.EmailGenerator.SendMail(web, sender1, user.Email, subject, bodyHTML, true, string.Empty, string.Empty);
+                        }
+                    }
+                    break;
+#endif
+                }
+            }
+
+        }
+
+        public static bool IsValidEmailAddress(string email)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(email);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+    }
+}
